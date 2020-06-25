27c29994f88ae95a8bc643cca64e50859df93a56
BabkovVasiliy <sharpestnoodle@gmail.com>
2020-06-25 22:37:19 +0300

    Исправлены утечки памяти и метод поиска

--- ListOfPositions.cpp
+++ ListOfPositions.cpp
@@ -1,6 +1,6 @@
 #include "ListOfPositions.h"
 #include <algorithm>
-/// Реализация методов класса ListOfPositions
+
 void ListOfPositions::Add(AbstractPosition* newPosition)
 {	
 	positions.push_back(newPosition);
@@ -13,7 +13,10 @@ void ListOfPositions::Remove(AbstractPosition* positionToRemove)
 		if (*pos == *positionToRemove) {
 			it = positions.erase(it);
 			delete(pos);
-			--it;
+			if (!positions.empty()&& it !=positions.begin())
+				--it;
+			if (positions.empty())
+				break;
 		}
 	}
 }
@@ -22,7 +25,7 @@ std::list<AbstractPosition*> ListOfPositions::Find(const std::string positionToF
 {
 	std::list<AbstractPosition*> found;
 	std::copy_if(positions.begin(), positions.end(), std::back_inserter(found),
-		[positionToFind](auto position) {return !position->GetValue().find(positionToFind); });
+		[positionToFind](auto position) {return (position->GetValue().find(positionToFind))!=std::string::npos; });
 	return found;
 }
 
@@ -33,8 +36,8 @@ std::list<AbstractPosition*> ListOfPositions::ReturnAll()
 
 ListOfPositions::~ListOfPositions()
 {
-	for (auto it = positions.begin(); it != positions.end(); ++it) {
-			it = positions.erase(it);
-			delete(*it);
+	for (const auto& pos : positions) {
+		delete(pos);
 	}
+	positions.clear();
 }
